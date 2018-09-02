0a2ece366e5a856a2293058a11b10658aeffae38
Matt Morgan <mcubed@outlook.com>
2018-09-02 13:06:05 -0500

    First Commit

--- Its Only Murder/Its Only Murder/Function1.cs
+++ Its Only Murder/Its Only Murder/Function1.cs
@@ -6,7 +6,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 
-namespace Its Only Murder
+namespace ItsOnlyMurder
 {
     public static class Function1
     {
